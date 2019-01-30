770a7ad4d40362b07a793b975ae0e3c61986ad23
rameshkalirawana <ramesh.kalirawana@outlook.com>
2019-01-30 13:50:42 +0530

    maven project

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,4 +1,11 @@
 /**
- * This is package info
- */
+* Domain classes used to produce .....
+* <p>
+* These classes contain the ......
+* </p>
+*
+* @since 1.0
+* @author somebody
+* @version 1.0
+*/
 package com.example;
